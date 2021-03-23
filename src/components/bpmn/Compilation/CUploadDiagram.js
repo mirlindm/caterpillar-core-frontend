a53544ac0d7653dc3144c63fd26098ee6f97fb9a
Mirlind Murati <mirlindmurati777@gmail.com>
2021-03-23 23:48:22 +0200

    Introducing Tabs to better handle the Deployment and Compilation Operations

--- src/components/bpmn/Compilation/CUploadDiagram.js
+++ src/components/bpmn/Compilation/CUploadDiagram.js
@@ -17,7 +17,7 @@ import 'react-notifications/lib/notifications.css';
 import {COMPILATION_URL} from '../../../Constants';
 
 
-import {Form, Alert, Button, Card, Accordion,} from 'react-bootstrap';
+import {Form, Alert, Button, Card, Accordion, Tab, Tabs} from 'react-bootstrap';
 
 import axios from 'axios';
 import {connect} from 'react-redux';
@@ -508,11 +508,12 @@ class CUploadDiagram extends Component {
                         </Alert>                        
                     :
                         // where the BPMN Model will be rendered if there is an uploaded diagram already! 
-                        <Aux>
-                           <Button className="new-buttons" variant="primary" type="submit" 
-                              style={{ marginBottom: "20px", width: "410px", marginLeft: "350px", marginRight: "350px", lineHeight: "35px", fontSize: "17px", fontWeight: "normal",}}>
-                              View Your Model
-                            </Button>
+                        <Aux>                                                                            
+                                <Button className="new-buttons" variant="primary" type="submit" 
+                                  style={{ marginBottom: "20px", width: "410px", marginLeft: "350px", marginRight: "350px", lineHeight: "35px", fontSize: "17px", fontWeight: "normal",}}>
+                                  View Your Model
+                                </Button>                                
+                            
                             <Card className="bg-gray-dark" style={{border: "2px solid #FFE4C4", width: "110%", marginLeft: "-60px", height: "100%"}}>                                              
                                 <div id="bpmncontainer">
                                     <div id="propview" style={{ width: '25%', height: '98vh', float: 'right', maxHeight: '98vh', overflowX: 'auto' }}></div>
@@ -533,19 +534,102 @@ class CUploadDiagram extends Component {
 
                   {/* New changes Start - POST 1 */}
                   <br/>
-                  <Card style={{border: "3px solid #FFE4C4", textAlign: "center"}}>
-                        <Alert variant="light" size="sm"> 
-                        <Button onClick={this.deployProcessModels}
-                                variant="primary" type="submit" className="new-buttons"
-                                style={{marginBottom: "5px", padding: "5px", lineHeight: "35px", fontSize: "17px",  fontWeight: "normal",}}> 
-                                Deploy Process Model
-                              </Button> {' '}
-                              <Button onClick={this.compileProcessModels}
-                                    variant="primary" type="submit" className="new-buttons"
-                                    style={{marginBottom: "5px", padding: "5px", lineHeight: "35px", fontSize: "17px",  fontWeight: "normal",}}
-                                    > Compile Process Model
-                              </Button>
-                        </Alert>  
+                  <Card style={{border: "3px solid #FFE4C4", textAlign: "center"}}>                        
+
+                          <Alert variant="light" size="sm" style={{display: "flex", justifyContent: "center"}}>
+                            <div> 
+                              <Tabs defaultActiveKey="profile" style={{display: "flex", justifyContent: "center"}} id="uncontrolled-tab-example">                             
+                                <Tab eventKey="profile" style={{color: "#E9967A"}} title="Proces Model Deployment"> <br/>
+                                  <Button onClick={this.deployProcessModels}
+                                      variant="primary" type="submit" className="new-buttons"
+                                      style={{marginBottom: "5px", marginTop: "10px", padding: "5px", lineHeight: "35px", fontSize: "17px",  fontWeight: "normal",}}> 
+                                      Deploy Process Model
+                                  </Button> <br/> <br/>
+                                  
+                                  {this.state.showIDAccordion ? 
+                                  <Accordion style={{marginBottom: "5px", padding: "5px", lineHeight: "35px", fontSize: "17px",  fontWeight: "normal",}}>
+                                    <Card>
+                                      <Card.Header>
+                                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                          <span style={{color: "#E9967A"}}> Model Bundle ID </span>
+                                        </Accordion.Toggle>
+                                      </Card.Header>
+                                      <Accordion.Collapse eventKey="0">
+                                        <Card.Body> <span style={{color: "#008B8B", fontWeight: "bold", fontSize: "17px", }}>  <pre> {this.state.id.length === 0 ? <span style={{color: "#FA8072"}}> Something went wrong. Please make sure your model is complete and has a correct name and try again ... </span> : this.state.id.bundleID} </pre> </span> </Card.Body>
+                                      </Accordion.Collapse>
+                                    </Card>            
+                                </Accordion>
+                                : null
+                                }
+
+                                </Tab>
+
+                                <Tab eventKey="contact" style={{color: "#E9967A"}} title="Process Model Compilation" > <br/>                                
+                                   <Button onClick={this.compileProcessModels}
+                                          variant="primary" type="submit" className="new-buttons"
+                                          style={{marginBottom: "5px", marginTop: "10px", padding: "5px", lineHeight: "35px", fontSize: "17px",  fontWeight: "normal",}}
+                                          > Compile Process Model
+                                  </Button> <br/> <br/>
+                                  <div style={{width: "70rem"}}>
+                                    {this.state.showCompileProcessModelsAccordion ?
+                                    <Accordion style={{marginBottom: "5px", padding: "5px", lineHeight: "35px", fontSize: "17px",  fontWeight: "normal",}}>
+                                      <Card>
+                                        <Card.Header>
+                                            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                              <span style={{color: "#E9967A"}}> Contract Name </span>
+                                            </Accordion.Toggle>
+                                          </Card.Header>
+                                          <Accordion.Collapse eventKey="0">
+                                            <Card.Body> <span style={{color: "#008B8B", fontWeight: "bold", fontSize: "17px", }}> <pre> {this.state.compileProcessModelsContractName.length === 0 ?  <span style={{color: "#FA8072"}}> Something went wrong. Please make sure your model is complete and has a correct name and try again ... </span> : this.state.compileProcessModelsContractName} </pre>  </span> </Card.Body>
+                                          </Accordion.Collapse>
+                                      </Card>
+                                      <Card>
+                                        <Card.Header>
+                                          <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                            <span style={{color: "#E9967A"}}> Solidity Code </span>
+                                          </Accordion.Toggle>
+                                          </Card.Header>
+                                            <Accordion.Collapse eventKey="1">
+                                            <Card.Body style={{textAlign: "center"}}> <span style={{color: "#008B8B", fontWeight: "bold", textAlign: "center", fontSize: "17px", }}> <pre> {this.state.compileProcessModelsSolidityCode === [] ? "Something went wrong!" : this.state.compileProcessModelsSolidityCode } </pre> </span> </Card.Body>
+                                            </Accordion.Collapse>
+                                      </Card>
+                                      <Card>
+                                        <Card.Header>
+                                          <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                                            <span style={{color: "#E9967A"}}> Code Dependencies </span>
+                                          </Accordion.Toggle>
+                                        </Card.Header>
+                                          <Accordion.Collapse eventKey="2">
+                                            <Card.Body style={{textAlign: "center"}}> 
+                                              Solidity Code 1: <span style={{color: "#008B8B", fontWeight: "bold", fontSize: "17px", textAlign: "center",  }}> <pre> {this.state.compileProcessModelsCodeDependencies[0]} </pre> </span> <hr/>
+                                              Solidity Code 2: <span style={{color: "#008B8B", fontWeight: "bold", fontSize: "17px", textAlign: "center",  }}> <pre> {this.state.compileProcessModelsCodeDependencies[1]} </pre> </span> <hr/>
+                                              Solidity Code 3: <span style={{color: "#008B8B", fontWeight: "bold", fontSize: "17px", textAlign: "center",  }}> <pre> {this.state.compileProcessModelsCodeDependencies[2]} </pre> </span> <hr/>
+                                              Solidity Code 4: <span style={{color: "#008B8B", fontWeight: "bold", fontSize: "17px", textAlign: "center",  }}> <pre> {this.state.compileProcessModelsCodeDependencies[3]} </pre> </span>                                         
+                                            </Card.Body>
+                                          </Accordion.Collapse>
+                                        </Card>
+
+                                        <Card>
+                                          <Card.Header>
+                                            <Accordion.Toggle as={Button} variant="link" eventKey="3">
+                                              <span style={{color: "#E9967A"}}> Compilation Metadata of Deployed Model </span>
+                                            </Accordion.Toggle>
+                                          </Card.Header>
+                                            <Accordion.Collapse eventKey="3">
+                                              <Card.Body> 
+                                                Contract Name: <span style={{color: "#008B8B", fontWeight: "bold", fontSize: "17px",}}> <pre> {this.state.compileProcessModelsCompilationMetadataContractName} </pre> </span> <hr/>
+                                                ABI: <span style={{color: "#008B8B", fontWeight: "bold", fontSize: "17px",}}> <br/> <pre>  {this.state.compileProcessModelsCompilationMetadataABI} </pre> </span> <hr/>
+                                                Byte Code: <span style={{color: "#008B8B", fontWeight: "bold", fontSize: "17px",}}> <br/> <pre> {this.state.compileProcessModelsCompilationMetadataByteCode} </pre> </span> <hr/>
+                                              </Card.Body>
+                                            </Accordion.Collapse>
+                                          </Card>                                                
+                                      </Accordion>                               
+                                  : null}
+                                </div>
+                                </Tab>
+                              </Tabs>                                                                                     
+                            </div> 
+                          </Alert> 
                         {/* <Card.Body> */}
                             {/* <Row style={{display: "flex", justifyContent: "space-around"}}>                                           
                               <Col>                                         */}
@@ -557,7 +641,7 @@ class CUploadDiagram extends Component {
                               {/* </Col>
                             </Row> */}
                          
-                              {this.state.showIDAccordion ? 
+                              {/* {this.state.showIDAccordion ? 
                                 <Accordion style={{marginBottom: "5px", padding: "5px", lineHeight: "35px", fontSize: "17px",  fontWeight: "normal",}}>
                                   <Card>
                                     <Card.Header>
@@ -625,7 +709,7 @@ class CUploadDiagram extends Component {
                                         </Accordion.Collapse>
                                       </Card>                                                
                                   </Accordion>                               
-                              : null}
+                              : null} */}
 
                                                                               
                         {/* </Card.Body> */}
